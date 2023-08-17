c3155df884e6bb56f3277179ab0d80d45275299a
Ole Specht <ole.specht@ifam.fraunhofer.de>
2023-08-17 11:11:36 +0200

    bootstrapform

--- web/forms.py
+++ web/forms.py
@@ -1,5 +1,17 @@
+from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 from .mixins import BootstrapMixin
 
 class LoginForm(BootstrapMixin, AuthenticationForm):
     pass
+
+class BootstrapForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({"class": "form-control"})
+            if type(field) == forms.fields.DateField:
+                field.widget.input_type = 'date'
+                field.widget.format = '%Y-%m-%d'
+
